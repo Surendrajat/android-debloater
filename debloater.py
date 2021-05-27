9b2a18732f9bdec5b282097ab6956857ae5db006
Surendrajat <surendrajat@protonmail.com>
2021-05-27 18:00:40 +0530

    test commit

--- debloater.py
+++ debloater.py
@@ -2,20 +2,23 @@ import re
 import os
 import subprocess
 
-def debloat():
-    with open('pkg.list', 'r') as p_file:
+
+def cleanup():
+    with open('pkg.list.redmi-note-10', 'r') as p_file:
         for line in p_file:
-            if str(line).startswith('-package'):
+            if str(line).startswith('-'):
                 apk = str(line).split(":")[1]
                 apk = str(apk).strip()
-                # uncomment below line to uninstall the bloat apps
-                cmd = "adb shell pm uninstall --user 0 {}".format(apk)
-                # uncomment below line to install bloat back if something goes wrong
-                # cmd = "adb shell cmd package install-existing {}".format(apk)
-                out_data = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE).stdout.read().decode()
+                if str(line).startswith('-package'):
+                    cmd = "adb shell pm uninstall --user 0 {}".format(apk)
+                    # cmd = "adb shell cmd package install-existing {}".format(apk)
+                else:
+                    cmd = "adb shell pm disable-user --user 0 {}".format(apk)
+                out_data = subprocess.Popen(
+                    cmd, shell=True, stdout=subprocess.PIPE).stdout.read().decode()
                 print("Uninstalled {}: {}".format(apk, out_data))
 
         p_file.close()
 
 
-debloat()
+cleanup()
